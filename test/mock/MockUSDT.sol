d7226fb920b535d550b8806d4623df0d689c030c
milad <miladbachari14@gmail.com>
2025-05-21 15:39:12 +0330

    feat: add mock usdt contract for test

---
+++ test/mock/MockUSDT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, Vm} from "forge-std/Test.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDT is ERC20 {
+    uint256 constant INITIAL_SUPPLY = 1000000000000000000000000;
+    uint8 constant DECIMALS = 18;
+
+    constructor() ERC20("Tether USDT", "USDT") {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function mint(address user, uint256 value) public {
+        _mint(user, value);
+    }
+}
