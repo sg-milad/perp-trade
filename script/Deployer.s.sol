a8a61db4134048a57f038d68bcee51b14fbfa0ab
milad <miladbachari14@gmail.com>
2025-05-27 11:58:10 +0330

    feat: deploy perp engin

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {LPToken} from "../src/LPToken.sol";
 import {Vault} from "../src/Vault.sol";
+import {PerpEngine} from "../src/PerpEngine.sol";
 import {MockUSDT} from "../test/mock/MockUSDT.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
 contract DeployerScript is Script {
+    uint256 constant INITIAL_BTC_PRICE = 50000e18; // $50,000
+
     LPToken public lPToken;
     Vault public vault;
     MockUSDT public mockUSDT;
+    PerpEngine public perpEngine;
+    MockV3Aggregator public priceFeed;
+    address deployer = makeAddr("deployer");
 
     function setUp() public {}
 
@@ -19,13 +26,26 @@ contract DeployerScript is Script {
         mockUSDT = new MockUSDT();
         lPToken = new LPToken();
         vault = new Vault(address(mockUSDT), address(lPToken));
+        priceFeed = new MockV3Aggregator(8, int256(INITIAL_BTC_PRICE / 1e10)); // Chainlink uses 8 decimals
 
+        perpEngine = new PerpEngine(address(vault), address(mockUSDT), address(priceFeed));
+
+        // Setup roles
         lPToken.grantMintAndBurnRole(address(vault));
+        vault.grantPerpEngineRole(address(perpEngine));
 
+        console.log("priceFeed: ", address(priceFeed));
+        console.log("perpEngine: ", address(perpEngine));
         console.log("lPToken: ", address(lPToken));
         console.log("vault: ", address(vault));
         console.log("mockUSDT: ", address(mockUSDT));
-
+        deposoitToValut();
         vm.stopBroadcast();
+    }
+
+    function deposoitToValut() public {
+        mockUSDT.mint(msg.sender, 1_000_000e6);
+        mockUSDT.approve(address(vault), 10_000e6);
+        vault.deposit(10_000e6);
     }
 }
